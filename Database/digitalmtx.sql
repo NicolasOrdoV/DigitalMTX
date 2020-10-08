7a7817751272a2ffea0ead77937ab2395b436de8
LeixerM <lgmolina39@misena.edu.co>
2020-10-07 20:53:05 -0500

    bd actualizada

--- Database/digitalmtx.sql
+++ Database/digitalmtx.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-10-2020 a las 23:05:01
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 08-10-2020 a las 03:51:39
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `garantias` (
   `id` int(11) NOT NULL,
+  `Numero_Factura` varchar(20) NOT NULL,
   `Nombre_Cliente` varchar(30) NOT NULL,
   `Telefono_Cliente` varchar(30) NOT NULL,
   `Correo_Cliente` varchar(30) NOT NULL,
@@ -39,6 +40,15 @@ CREATE TABLE `garantias` (
   `Estado` varchar(20) NOT NULL,
   `id_Personal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `garantias`
+--
+
+INSERT INTO `garantias` (`id`, `Numero_Factura`, `Nombre_Cliente`, `Telefono_Cliente`, `Correo_Cliente`, `Direccion_Cliente`, `Observacion_Cliente`, `Observacion_Empleado`, `Aprobacion_Garantia`, `Estado`, `id_Personal`) VALUES
+(2, '', 'Juan', '34343', 'Juan@email.com', 'Calle 100', 'El celular tiene la bateria dañada', 'Recibo bateria dañada', 'SI', 'Pendiente', 1),
+(3, '432432432', 'Juan', '2321321', 'Jnordonez7@misena.edu.co', 'Calle 100', 'sadasdas', 'dasdas', 'SI', 'Pendiente', 1),
+(4, '1930024', 'Diego', '3242342', 'fredyvgr@hotmail.com', 'Cra 47 N75 S64', 'Bateria en mal estado', 'Se reviso y se paso a soporte tecnico', 'SI', 'Pendiente', 1);
 
 -- --------------------------------------------------------
 
@@ -51,10 +61,17 @@ CREATE TABLE `personal` (
   `Nombres` varchar(30) NOT NULL,
   `Apellidos` varchar(30) NOT NULL,
   `Correo` varchar(30) NOT NULL,
-  `Contraseña` varchar(30) NOT NULL,
+  `Contrasena` varchar(30) NOT NULL,
   `Telefono` varchar(30) NOT NULL,
   `id_rol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `personal`
+--
+
+INSERT INTO `personal` (`id`, `Nombres`, `Apellidos`, `Correo`, `Contrasena`, `Telefono`, `id_rol`) VALUES
+(1, 'Fredy', 'Belson', 'fredy@gmail.com', '123456', '55555555', 3);
 
 -- --------------------------------------------------------
 
@@ -108,13 +125,13 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `garantias`
 --
 ALTER TABLE `garantias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `personal`
 --
 ALTER TABLE `personal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
