e7410f1aa30806fa6d0e5780c653a9e9209934c0
NicolasOrdoV <jnordonez7@misena.edu.co>
2020-10-25 16:32:46 -0500

    Departamentos

--- Views/Garanty/datos.php
+++ Views/Garanty/datos.php
@@ -1,20 +1,22 @@
 <?php 
 $conexion=mysqli_connect('localhost','root','','digitalmtx_dtmmtx');
+//mysql_set_charset('utf8',$conexion);
 $municipio=$_POST['Municipio'];
 
 	$sql="SELECT d.id,d.Departamento,m.Municipio FROM mg_departamentos d , mg_municipio m
-                WHERE d.id = m.id_departamento AND d.Departamento = $municipio";
+                WHERE d.id = m.id_departamento AND d.Departamento = '$municipio'";
 
 	$result=mysqli_query($conexion,$sql);
 
 	$cadena="<p>
                 <b>Municipio</b>
             </p>
-            <input list='strets' id='lista2' name='Municipio' placeholder='Busca el municipio*' required>
+            <div class='form-group form-float'>
+            <input list='strets' id='lista2' name='Municipio' class='form-control' placeholder='Busca el municipio*'>
                     <datalist id='strets'>";
 
 	while ($ver=mysqli_fetch_row($result)) {
-		$cadena=$cadena.'<option>' .$ver[2].'-'.utf8_encode($ver[2]).'</option>';
+		$cadena=$cadena.'<option value='.utf8_encode($ver[2]).'>'.utf8_encode($ver[2]).'</option>';
 	}
 
-	echo  $cadena."</datalist><div class='valid-feedback'>Valido".var_dump($result)."</div>";
+	echo  $cadena."</datalist></div>";
