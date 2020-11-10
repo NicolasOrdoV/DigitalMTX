683b4f9762726101bc5f568d3c1223f829be87c3
NicolasOrdoV <jnordonez7@misena.edu.co>
2020-11-09 21:54:43 -0500

    Arreglos menores

--- Models/Client.php
+++ Models/Client.php
@@ -19,7 +19,7 @@ class Client
 	public function getAll()
 	{
 		try {
-			$strSql = "SELECT * FROM mg_clientes LIMIT 4000";
+			$strSql = "SELECT * FROM mg_clientes ORDER BY IDENTIFICACION DESC LIMIT 4000";
 			$query = $this->pdo->select($strSql);
 			return $query;
 		} catch (PDOException $e) {
