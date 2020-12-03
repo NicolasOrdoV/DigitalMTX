39839593baa5d146a32a988065aa1d593e19c6aa
NicolasOrdoV <jnordonez7@misena.edu.co>
2020-12-02 22:50:14 -0500

    Arreglos menores

--- Controllers/PersonController.php
+++ Controllers/PersonController.php
@@ -88,7 +88,7 @@ class PersonController
 				$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
 				$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 				$mail->Username   = 'nikomegathet666@gmail.com';                     // SMTP username
-				$mail->Password   = '1000464327bat';                               // SMTP password
+				$mail->Password   = 'batman1000464327';                               // SMTP password
 				$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 				$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
